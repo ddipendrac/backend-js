4906fdcd777a38d6bbe07328ed2539caf23ba7fc
Dipendra Chaudhary <ddipendrac@gmail.com>
2025-02-22 14:51:29 +0545

    all setup configured

--- src/app.js
+++ src/app.js
@@ -18,4 +18,14 @@ app.use(express.static("public"))
 // read users cookie
 app.use(cookieParser())
 
+//routes import 
+import userRouter from './routes/user.routes.js'
+
+// routes declare
+app.use("/api/v1/users", userRouter)
+
+
+
+// http://localhost:8000/api/v1/users/register
+
 export { app }
